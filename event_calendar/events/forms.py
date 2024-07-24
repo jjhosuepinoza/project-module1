b851085a8eb54adfbe146e584261ddc521d35287
jjhosuepinoza <97245422+jjhosuepinoza@users.noreply.github.com>
2024-07-24 09:59:31 +0200

    styled form

--- event_calendar/events/forms.py
+++ event_calendar/events/forms.py
@@ -8,5 +8,23 @@ class VenueForm(ModelForm):
     class Meta:
         model = Venue 
         fields = ('name', 'address', 'zip_code', 'phone', 'email_address', 'web' )
+        widgets = {
+            'name': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Venue Name' }), 
+            'address': forms.TextInput(attrs={'class':'form-control' , 'placeholder':'Address'}), 
+            'zip_code': forms.TextInput(attrs={'class':'form-control' , 'placeholder':'Zip Code'}), 
+            'phone': forms.TextInput(attrs={'class':'form-control' , 'placeholder':'Phone'}), 
+            'email_address': forms.TextInput(attrs={'class':'form-control' , 'placeholder':'Email'}), 
+            'web': forms.TextInput(attrs={'class':'form-control' , 'placeholder':'Web Url'})
+            }
         
-  
+        labels =  {
+        'name': '', 
+        'address': '', 
+        'zip_code': '', 
+        'phone': '', 
+        'email_address': '', 
+        'web': ''
+    }
+
+
+
