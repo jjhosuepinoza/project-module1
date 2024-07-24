42a5fd0cfcab943a892707665dc939924c89670a
jjhosuepinoza <97245422+jjhosuepinoza@users.noreply.github.com>
2024-07-24 07:35:19 +0200

    first commit

---
+++ event_calendar/events/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from django.forms import ModelForm
+from .models import Venue
+
+#Create a venue form
+
+class VenueForm(ModelForm):
+    class Meta:
+        model = Venue 
+        fields = ('name', 'address', 'zip_code', 'phone', 'email_address', 'web' )
+        
+    #     labels =  {
+    #     'name': '', 
+    #     'address': '', 
+    #     'zip_code': '', 
+    #     'phone': '', 
+    #     'email_address': '', 
+    #     'web': ''
+    # }
+
+
+    # widgets = {
+    #     'name': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Venue Name' }), 
+    #     'address': forms.TextInput(attrs={'class':'form-control' , 'placeholder':'Address'}), 
+    #     'zip_code': forms.TextInput(attrs={'class':'form-control' , 'placeholder':'Zip Code'}), 
+    #     'phone': forms.TextInput(attrs={'class':'form-control' , 'placeholder':'Phone'}), 
+    #     'email_address': forms.TextInput(attrs={'class':'form-control' , 'placeholder':'Email'}), 
+    #     'web': forms.TextInput(attrs={'class':'form-control' , 'placeholder':'Web Url'})
+    # }
